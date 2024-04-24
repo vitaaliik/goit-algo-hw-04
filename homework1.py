9583d7b4d06184ac132d949694d1009467c4502f
vitalik <vitaliklutchak12@gmail.com>
2024-04-24 20:36:22 +0300

    dz

---
+++ homework1.py
@@ -0,0 +1,19 @@
+def total_salary(path):
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            total_salary = 0
+            for line in lines:
+                name, salary = line.strip().split(',')
+                total_salary += int(salary)
+            average_salary = int(total_salary / len(lines))
+            return total_salary, average_salary
+    except FileNotFoundError:
+        print(f"Файл {path} не знайдено.")
+    except Exception as e:
+        print(f"Сталася помилка: {e}")
+
+# Приклад використання:
+total, average = total_salary("sal.txt")
+if total is not None and average is not None:
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
